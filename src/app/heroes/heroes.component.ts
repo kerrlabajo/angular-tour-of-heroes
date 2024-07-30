236a0a86d28a8b32cfa64935fe484dd84692c2f7
Kerr Labajo <klabajo@magaya.com>
2024-07-30 08:37:42 +0800

    Import and inject `MessageService`

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,7 +14,10 @@ export class HeroesComponent implements OnInit{
   //   name: 'Windstorm'
   // };
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   heroes: Hero[] = [];
   selectedHero?: Hero;
