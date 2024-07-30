e9995c64dfac465595f987c0b10cd4de300a1d5c
Kerr Labajo <klabajo@magaya.com>
2024-07-30 09:59:39 +0800

    Import `HttpClientInMemoryWebApiModule` and `InMemoryDataService`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { NgIf } from '@angular/common';
 import { UpperCasePipe } from '@angular/common';
 
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,6 +32,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     NgIf,
     UpperCasePipe,
     HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [
     provideClientHydration()
