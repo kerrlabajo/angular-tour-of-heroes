ed4cac83525d6f8a9f08fc40a146c6892ad8552d
Kerr Labajo <klabajo@magaya.com>
2024-07-30 08:26:08 +0800

    Create `add()` and `clear()` methods

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -6,5 +6,11 @@ import { Injectable } from '@angular/core';
 export class MessageService {
   messages: string[] = [];
 
-  constructor() { }
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
 }
