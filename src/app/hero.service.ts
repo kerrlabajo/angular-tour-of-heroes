847e97b5c84c13223860625c9e44c6bd4faf64d6
Kerr Labajo <klabajo@magaya.com>
2024-07-30 10:15:04 +0800

    Use `Observable.pipe() to `catchError()` and implement `handleError()` method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -27,11 +27,35 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     this.log("fetched heroes");
     return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
     this.log(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
