631070286615d2fbec1baa90bc1cb5557fcb0f71
Kerr Labajo <klabajo@magaya.com>
2024-07-30 10:09:59 +0800

    Refactor to use `log()` method instead

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -30,7 +30,7 @@ export class HeroService {
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`HeroService: fetched hero id=${id}`);
     return of(hero);
   }
 }
