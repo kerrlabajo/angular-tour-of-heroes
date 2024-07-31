0559eb7eb97463b816463d91cd894d2b5a1112dd
Kerr Labajo <klabajo@magaya.com>
2024-07-31 18:49:18 +0800

    Use `sharedHeroes` from parent component to avoid many api requests

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { map, Observable, Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -11,6 +11,8 @@ import { HeroService } from '../hero.service';
 })
 export class HeroSearchComponent implements OnInit{
   heroes$!: Observable<Hero[]>;
+  @Input() sharedHeroes!: Hero[];
+
   private searchTerms = new Subject<string>();
 
   constructor(private heroService: HeroService) {}
@@ -23,13 +25,30 @@ export class HeroSearchComponent implements OnInit{
   ngOnInit(): void {
     this.heroes$ = this.searchTerms.pipe(
       // wait 300ms after each keystroke before considering the term
-      debounceTime(300),
+      debounceTime(100),
 
       // ignore new term if same as previous term
       distinctUntilChanged(),
 
+      // map the search term to a filtered list of sharedHeroes
+      map((term: string) => this.filterHeroes(term)),
+
       // switch to new search observable each time the term changes
-      switchMap((term: string) => this.heroService.searchHeroes(term)),
+      // switchMap((term: string) => this.heroService.searchHeroes(term)),
+    );
+  }
+
+  private filterHeroes(term: string): Hero[] {
+    if (!term.trim()) {
+      // if no search term, return all sharedHeroes
+      return [];
+    }
+    if (!isNaN(Number(term))) {
+      term = term.trim();
+      return this.sharedHeroes.filter(hero => hero.id.toString().includes(term));
+    }
+    return this.sharedHeroes.filter(hero =>
+      hero.name.toLowerCase().includes(term.toLowerCase())
     );
   }
 }
