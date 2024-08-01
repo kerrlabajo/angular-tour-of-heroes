112fedbf3fbe6b05da84e96781ce8b8bd6f78e29
Kerr Labajo <klabajo@magaya.com>
2024-08-01 08:25:50 +0800

    Create var `loading` and its setters/getters

--- src/app/loader.service.ts
+++ src/app/loader.service.ts
@@ -5,5 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class LoaderService {
 
+  private loading: boolean = false;
+
   constructor() { }
+
+  setLoading(loading: boolean) {
+    this.loading = loading;
+  }
+
+  getLoading(): boolean {
+    return this.loading;
+  }
 }
