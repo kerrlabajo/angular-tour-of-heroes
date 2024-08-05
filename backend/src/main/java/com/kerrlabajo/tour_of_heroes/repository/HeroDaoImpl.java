fc38c1258528a44c91f278087c9d4f18970d905d
Kerr Labajo <klabajo@magaya.com>
2024-08-05 17:28:51 +0800

    Move to  folder

---
+++ backend/src/main/java/com/kerrlabajo/tour_of_heroes/repository/HeroDaoImpl.java
@@ -0,0 +1,44 @@
+package com.kerrlabajo.tour_of_heroes.repository;
+
+import com.kerrlabajo.tour_of_heroes.model.Hero;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class HeroDaoImpl implements HeroDao{
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Hero> getAllHeroes() {
+        return jdbcTemplate.query(
+                "SELECT * FROM heroes",
+                new BeanPropertyRowMapper<>(Hero.class)
+        );
+    }
+
+    @Override
+    public Hero getHeroById(int id) {
+        return null;
+    }
+
+    @Override
+    public Hero addHero(Hero hero) {
+        return null;
+    }
+
+    @Override
+    public Hero updateHero(Hero hero) {
+        return null;
+    }
+
+    @Override
+    public Hero deleteHero(int id) {
+        return null;
+    }
+}
---
+++ backend/src/test/java/com/kerrlabajo/tour_of_heroes/TourOfHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerrlabajo.tour_of_heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourOfHeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
