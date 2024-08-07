067c5ba723f6f84d35b1a51ab468b81fec4d1691
Kerr Labajo <klabajo@magaya.com>
2024-08-07 09:01:54 +0800

    Add `getHeroById` controller

--- backend/src/main/java/com/kerrlabajo/tour_of_heroes/controller/HeroController.java
+++ backend/src/main/java/com/kerrlabajo/tour_of_heroes/controller/HeroController.java
@@ -4,10 +4,7 @@ import com.kerrlabajo.tour_of_heroes.repository.HeroDao;
 import com.kerrlabajo.tour_of_heroes.model.Hero;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +20,10 @@ public class HeroController {
     @GetMapping
     public List<Hero> getHeroes() {
         return heroDao.getAllHeroes();
+    }
+
+    @GetMapping(params = "id")
+    public Hero getHeroById(@RequestParam int id) {
+        return heroDao.getHeroById(id);
     }
 }
